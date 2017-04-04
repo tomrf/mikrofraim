32c3d5091265e7dc82611e65121239eb29a30db7
Tom Rune Flo <tom@x86.no>
2017-04-04 09:59:01 +0200

    add monolog

---
+++ lib/Log.php
@@ -0,0 +1,41 @@
+<?php
+
+class Log
+{
+    private static $logger = null;
+
+    public static function init()
+    {
+        $formatter = new \Monolog\Formatter\LineFormatter("%datetime% > %level_name% > %message%\n");
+        $stream = new \Monolog\Handler\StreamHandler('../' . getenv('LOG_FILENAME'), \Monolog\Logger::DEBUG);
+        $stream->setFormatter($formatter);
+        self::$logger = new \Monolog\Logger('_');
+        self::$logger->pushHandler($stream);
+    }
+
+    public static function debug($message)
+    {
+        if (self::$logger) self::$logger->debug($message);
+    }
+
+    public static function info($message)
+    {
+        if (self::$logger) self::$logger->info($message);
+    }
+
+    public static function notice($message)
+    {
+        if (self::$logger) self::$logger->notice($message);
+    }
+
+    public static function warning($message)
+    {
+        if (self::$logger) self::$logger->warning($message);
+    }
+
+    public static function error($message)
+    {
+        if (self::$logger) self::$logger->error($message);
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -2,24 +2,43 @@
     /* autoload vendor packages */
     require_once('../vendor/autoload.php');
 
-    /* read .env */
+    /* load environment variables from .env */
     if (! file_exists('../.env')) {
         die('<b>Error:</b> Environment file ".env" missing.<br>Rename ".env.example" to ".env" in the project root, and edit the file as necessary.');
     }
     $dotenv = new Dotenv\Dotenv('../');
     $dotenv->load();
 
-    /* register whoops handler if enabled */
-    if (filter_var(getenv('USE_WHOOPS'), FILTER_VALIDATE_BOOLEAN)) {
-        $whoops = new \Whoops\Run;
-        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-        $whoops->register();
+    /* set production mode */
+    if (filter_var(getenv('PRODUCTION'), FILTER_VALIDATE_BOOLEAN)) {
+        ini_set('display_errors','Off');
+    } else {
+        /* register whoops handler if enabled */
+        if (filter_var(getenv('USE_WHOOPS'), FILTER_VALIDATE_BOOLEAN)) {
+            $whoops = new \Whoops\Run;
+            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+            $whoops->register();
+        }
     }
 
     /* require local framework components */
-    require_once('../lib/View.php');
     require_once('../lib/Router.php');
     require_once('../lib/RouterResponse.php');
+    require_once('../lib/View.php');
+    require_once('../lib/Log.php');
+
+    /* set up monolog */
+    // $log = new \Monolog\Logger('name');
+    // $log->pushHandler(new \Monolog\Handler\StreamHandler('../' . getenv('LOG_FILENAME'), \Monolog\Logger::DEBUG));
+    // $log->warning('Fooffff');
+    // $log->error('Barrrrr');
+    // $log->info('abklabkabk abk akbakbk a123 123 123');
+    if (filter_var(getenv('USE_MONOLOG'), FILTER_VALIDATE_BOOLEAN)) {
+        Log::init();
+        Log::debug('yay');
+        Log::notice('yay2');
+        Log::warning('yay3');
+    }
 
     /* create Router instance, load routes from ../routes.php */
     $router = new Router();
