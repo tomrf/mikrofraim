78693733becd9d410a683fb86eb06f6f9a4ae7e4
Tom Rune Flo <tom@x86.no>
2017-04-06 01:58:13 +0200

    tab->spaces fix

--- routes.php
+++ routes.php
@@ -1,8 +1,8 @@
 <?php
 
-	/* example route showing optional route arguments and closure handler */
+    /* example route showing optional route arguments and closure handler */
     $router->routeAdd('GET', '/test/{val?}', function($val = 'default') {
-    	return "val = $val";
+        return "val = $val";
     });
 
     /* default home route with controller class handler */
